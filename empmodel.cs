1c579d0db2acdf83e54919989894806ae9b96e47
Rahul kumar <78337923+rahul-chauhan6@users.noreply.github.com>
2022-03-11 13:02:24 +0530

    Add files via upload

---
+++ empmodel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rahul_firstMVC.Models
+{
+    public class empmodel
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+    }
+}
